c685d07ddd0358712a64aedc1783628d4d569653
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-02 14:33:18 -0300

    agrego Box y Row para ordenar iconos

--- app/src/main/java/com/example/instagramproject/profile/components/ProfileHeader.kt
+++ app/src/main/java/com/example/instagramproject/profile/components/ProfileHeader.kt
@@ -1,5 +1,7 @@
 package com.example.instagramproject.profile.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
@@ -10,21 +12,36 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileHeader() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "arrow back")
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+        Box {
+            Row (verticalAlignment = Alignment.CenterVertically) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "arrow back")
+                }
+                Text(text = "odikil999")
+            }
         }
-        Text(text = "odikil999")
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Default.Notifications, contentDescription = "notifications")
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Default.MoreVert, contentDescription = "options")
+
+        Box {
+            Row {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "notifications")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.MoreVert, contentDescription = "options")
+                }
+            }
         }
     }
 }
