a2ac8dd5bcca51479e42fc2c0a28e0b854d85ef7
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-02 15:29:24 -0300

    agrego ProfileHeader a la pantalla principal

--- app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
@@ -2,9 +2,15 @@ package com.example.instagramproject.profile
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.instagramproject.profile.components.ProfileHeader
 
 @Composable
 fun ProfileScreen() {
+    ProfileHeader(
+        backClick = {},
+        notificationClick = {},
+        optionsClick = {}
+    )
 
 }
 
