a2c3c1bb428a9aae5aabb012e54bfff1d204a272
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-02 14:22:23 -0300

    agrego icono notificaciones

--- app/src/main/java/com/example/instagramproject/profile/components/ProfileHeader.kt
+++ app/src/main/java/com/example/instagramproject/profile/components/ProfileHeader.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,7 +17,14 @@ import androidx.compose.ui.tooling.preview.Preview
 fun ProfileHeader() {
     Row(modifier = Modifier.fillMaxWidth()) {
         IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "arrow back")
+        }
+        Text(text = "odikil999")
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Notifications, contentDescription = "notifications")
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.MoreVert, contentDescription = "options")
         }
     }
 }
