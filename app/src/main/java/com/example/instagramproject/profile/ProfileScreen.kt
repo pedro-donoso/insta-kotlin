0b6e81bc8c35dbbe544cd70eadb4df1457d46b6e
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-02 14:05:38 -0300

    creo profile

--- app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
@@ -1,2 +1,15 @@
 package com.example.instagramproject.profile
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfileScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
