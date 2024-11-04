9451c8f980966e06ea7f87b8b6fbd17266478fc7
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-04 12:46:27 -0300

    creo data class story

--- app/src/main/java/com/example/instagramproject/profile/components/ProfileStory.kt
+++ app/src/main/java/com/example/instagramproject/profile/components/ProfileStory.kt
@@ -1,2 +1,21 @@
 package com.example.instagramproject.profile.components
 
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfileStory(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(modifier = modifier) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileStoryPreview() {
+    ProfileStory()
+}
