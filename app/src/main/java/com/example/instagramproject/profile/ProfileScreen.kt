563c2d739beac87445c7d2dcf3a72f3c99763b27
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-03 21:19:05 -0300

    agrego padding

--- app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramproject/profile/ProfileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.instagramproject.profile.components.ProfileDescription
 import com.example.instagramproject.profile.components.ProfileHeader
 import com.example.instagramproject.profile.components.ProfileInformation
 
@@ -24,6 +25,9 @@ fun ProfileScreen() {
             optionsClick = {}
         )
         ProfileInformation()
+        ProfileDescription(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 4.dp)
+        )
     }
 }
 
--- app/src/main/java/com/example/instagramproject/profile/components/ProfileDescription.kt
+++ app/src/main/java/com/example/instagramproject/profile/components/ProfileDescription.kt
@@ -1,16 +1,20 @@
 package com.example.instagramproject.profile.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ProfileDescription() {
-    Column {
+fun ProfileDescription(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
         Text(text = "Pedro", fontWeight = FontWeight.Bold)
-        Text(text = "Description")
+        Text(text = "Descripción")
     }
 }
 
