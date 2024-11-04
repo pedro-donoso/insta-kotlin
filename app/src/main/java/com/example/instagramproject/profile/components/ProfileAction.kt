68e90769f2b78d7affe35fe0104781ca28c7de52
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-04 12:07:17 -0300

    agrego botones

--- app/src/main/java/com/example/instagramproject/profile/components/ProfileAction.kt
+++ app/src/main/java/com/example/instagramproject/profile/components/ProfileAction.kt
@@ -2,23 +2,35 @@ package com.example.instagramproject.profile.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProfileAction(
     modifier: Modifier = Modifier
 ) {
     Row(modifier = modifier.fillMaxWidth()) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Seguir")
-            }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Mensaje")
-        }
+        ProfileButton(onClick = {}, text = "Seguir")
+        ProfileButton(onClick = {}, text = "Mensaje")
+    }
+}
+
+@Composable
+private fun ProfileButton(onClick: () -> Unit, text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray),
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier
+    ) {
+        Text(text = text)
     }
 }
 
